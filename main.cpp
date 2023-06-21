4d91b2909bcefd75a589d7de9f8f00f807b8411b
Novak <novaldix@gmail.com>
2023-06-21 20:45:41 +0300

    19_5_4 improved: added cycle to main and descr to readme

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 
 using std::cout;
 using std::endl;
+using std::string;
+using std::vector;
 
 // Присутствует ли в пути расширение png
 bool hasFileExtensionPNG(const std::string &pathName) {
@@ -28,6 +30,7 @@ bool hasFileExist(const char* fileName) {
     return isExist;
 }
 
+// Считывает первую половину заголовка. Если она соответствует правилам, то это файл PNG
 bool checkHeaderPNG(const char* fileName) {
     long long headerSize = 8 / 2;
     char buffer[headerSize];
@@ -35,6 +38,7 @@ bool checkHeaderPNG(const char* fileName) {
     std::ifstream fileReader;
     fileReader.open(fileName, std::ios::binary);
     fileReader.read(buffer, headerSize);
+    fileReader.seekg(0, std::ios::beg);
     fileReader.close();
 
     return (((buffer[0] - 0) == -119) && ((buffer[1]) == 'P') && ((buffer[2]) == 'N') && ((buffer[3]) == 'G'));
@@ -44,22 +48,28 @@ int main() {
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    // const char* pathName = R"(..\136.png)";
-    const char* pathName = R"(..\test.co.png)";
-    // const char* pathName = R"(..\test.co.txt)";
-    bool isExist = hasFileExist(pathName);
+    vector<string> paths = { R"(..\136.pngX)", R"(..\test.co.png)", R"(..\test.co.txt)", R"(..\test)" };
 
-    if (!isExist) {
-        cout << "Такого файла не существует. Попробуйте снова" << endl;
+    for (const string &pathName : paths) {
+        bool isExist = hasFileExist(pathName.c_str());
+        bool isFileExtensionPNG = hasFileExtensionPNG(pathName);
+
+        if (isExist && isFileExtensionPNG) {
+            bool isHeaderPNG = checkHeaderPNG(pathName.c_str());
+            if (isHeaderPNG) {
+                printf("Файл %s обнаружен в текущей директории и его можно открывать\n", pathName.c_str());
+            } else {
+                printf("Файл %s обнаружен, но с ним какие-то проблемы (неверный заголовок)\n", pathName.c_str());
+            }
+        }
+
+        if (!isExist) {
+            printf("Файл %s не обнаружен в текущей директории\n", pathName.c_str());
+        }
+
+        if (!isFileExtensionPNG) {
+            printf("У файла %s должно быть расширение .png\n", pathName.c_str());
+        }
     }
 
-    bool isFileExtensionPNG = hasFileExtensionPNG(pathName);
-
-    if (!isFileExtensionPNG) {
-        cout << "У файла должно быть расширение .png" << endl;
-    }
-
-    if (isExist && isFileExtensionPNG) {
-        cout << checkHeaderPNG(pathName) << endl;
-    }
 }
