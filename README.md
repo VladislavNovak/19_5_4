7794f9a847cfe3dbe4094966d19668f13651f8c5
Novak <novaldix@gmail.com>
2023-06-21 18:12:32 +0300

    19_5_4 added solution

--- README.md
+++ README.md
@@ -18,3 +18,12 @@
 
 Чтобы определить расширение файла, используйте функцию substr, 
 которая получает офсет и длину подстроки и возвращает её.
+
+
+## <font color="#tomato">Полезные ссылки</font>
+
+[Как получить расширение файла из строки в C ++](https://overcoder.net/q/36811/%D0%BA%D0%B0%D0%BA-%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B8%D1%82%D1%8C-%D1%80%D0%B0%D1%81%D1%88%D0%B8%D1%80%D0%B5%D0%BD%D0%B8%D0%B5-%D1%84%D0%B0%D0%B9%D0%BB%D0%B0-%D0%B8%D0%B7-%D1%81%D1%82%D1%80%D0%BE%D0%BA%D0%B8-%D0%B2-c)
+
+[std::basic_string::rfind](https://runebook.dev/ru/docs/cpp/string/basic_string/rfind)
+
+[Преобразовать файл в двоичный код](https://hexed.it/)
--- main.cpp
+++ main.cpp
@@ -1,5 +1,65 @@
+#include <windows.h> // SetConsoleCP
 #include <iostream>
+#include <fstream>
+#include <vector>
+
+using std::cout;
+using std::endl;
+
+// Присутствует ли в пути расширение png
+bool hasFileExtensionPNG(const std::string &pathName) {
+    size_t position = pathName.find_last_of('.');
+
+    if (position != std::string::npos)
+        if (pathName.substr(position + 1) == "png")
+            return true;
+
+    return false;
+}
+
+// Существует ли в принципе, искомый файл
+bool hasFileExist(const char* fileName) {
+    bool isExist = false;
+    std::ifstream fileReader;
+    fileReader.open(fileName);
+    if (fileReader.is_open() && !fileReader.bad()) isExist = true;
+    fileReader.close();
+
+    return isExist;
+}
+
+bool checkHeaderPNG(const char* fileName) {
+    long long headerSize = 8 / 2;
+    char buffer[headerSize];
+
+    std::ifstream fileReader;
+    fileReader.open(fileName, std::ios::binary);
+    fileReader.read(buffer, headerSize);
+    fileReader.close();
+
+    return (((buffer[0] - 0) == -119) && ((buffer[1]) == 'P') && ((buffer[2]) == 'N') && ((buffer[3]) == 'G'));
+}
 
 int main() {
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
 
+    // const char* pathName = R"(..\136.png)";
+    const char* pathName = R"(..\test.co.png)";
+    // const char* pathName = R"(..\test.co.txt)";
+    bool isExist = hasFileExist(pathName);
+
+    if (!isExist) {
+        cout << "Такого файла не существует. Попробуйте снова" << endl;
+    }
+
+    bool isFileExtensionPNG = hasFileExtensionPNG(pathName);
+
+    if (!isFileExtensionPNG) {
+        cout << "У файла должно быть расширение .png" << endl;
+    }
+
+    if (isExist && isFileExtensionPNG) {
+        cout << checkHeaderPNG(pathName) << endl;
+    }
 }
